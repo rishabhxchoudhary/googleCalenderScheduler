0d291d23b28f40c89d2e2b519213e26545e66a55
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-06-22 15:10:28 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ app.use(verifyLoggedIn,calendarRoutes);
 
 const PORT = process.env.PORT || 8000;
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Google Calendar API!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
